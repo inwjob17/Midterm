e78abbb4d06ba0156c6e779d48b1960af6348c3a
inwjob17 <inwjob17@gmail.com>
2026-03-26 08:18:10 +0700

    1.3

--- Assets/Script/DeadZone.cs
+++ Assets/Script/DeadZone.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeadZone : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+void OnTriggerEnter(Collider other)
+{
+	SceneManager.LoadScene("Map");
+}
 }
